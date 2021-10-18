7c1c6d4934a264b8fa4e3c39cdf12e6b99f0aa3d
anshumanyadavhoabl <92367573+anshumanyadavhoabl@users.noreply.github.com>
2021-10-18 16:11:47 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+ module.exports = {
+    port: process.env.PORT || 5001,
+    facebookClientId: process.env.facebookClientId || 0000000000,
+    facebookClientSecret: process.env.facebookClientSecret || "",
+    privateTokenForJWT: "  Dol9aUiBGT3u2KjQzUkfU3RbBomsWrMP",
+    graphqlUrl: process.env.graphqlUrl || '',
+};
